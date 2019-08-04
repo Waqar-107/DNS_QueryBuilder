c1502a017341b52d027f88ad21487d66904caa6e
Waqar-107 <waqar.hassan866@gmail.com>
2019-08-04 11:54:08 +0600

    updated

--- DNS_QueryBuilder.py
+++ DNS_QueryBuilder.py
@@ -3,20 +3,19 @@
 import socket
 import binascii
 
-
 class dns_query:
-    def __init__(self, server_ip, port):
-        self.server_ip = server_ip
-        self.port = port
-        self.msg = ""
+    def __init__(self):
         self.query_type = 0
         self.query_no = 1
         self.recursiveQuery = True
         self.truncated = False
         self.questionQuantity = 1
+        self.domain_name = "-1"
+        self.showReport = False
+        self.showError = False
 
     # the binary representation will be returned, if the length is < desired digits then extra 0 is padded
-    def getBinary(self, number, digit):
+    def __getBinary(self, number, digit):
         s = ""
         while number > 0:
             s += chr(number % 2 + ord('0'))
@@ -36,21 +35,29 @@ class dns_query:
     def setRecusiveQuery(self, flag):
         self.recursiveQuery = flag
 
-    # set the number of questions
-    def setQuestionQuantity(self, q):
-        self.questionQuantity = q
-
-    def buildHeader(self):
+    # set true or false - false by default
+    def setShowReport(self, flag):
+        self.showReport = flag
+    
+    # set true or false - false by default
+    def setShowError(self, flag):
+        self.showError = flag
+        
+    # set domain name
+    def setDomainName(self, name):
+        self.domain_name = name
+    
+    def __buildHeader(self):
         header = ""
 
         # same will be returned in the response of the query - 16 bits
-        ID = self.getBinary(self.query_no, 16)
+        ID = self.__getBinary(self.query_no, 16)
 
         # 1 4 1 1 1 1 3 4
         # |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
         QR = "0"
 
-        opcode = self.getBinary(self.query_type, 4)
+        opcode = self.__getBinary(self.query_type, 4)
 
         AA = "0"
         TC = "1" if self.truncated else "0"
@@ -61,11 +68,11 @@ class dns_query:
         RCODE = "0000"
 
         # number of questions
-        QDCOUNT = self.getBinary(self.questionQuantity, 16)
+        QDCOUNT = self.__getBinary(self.questionQuantity, 16)
 
-        ANCOUNT = self.getBinary(0, 16)
-        NSCOUNT = self.getBinary(0, 16)
-        ARCOUNT = self.getBinary(0, 16)
+        ANCOUNT = self.__getBinary(0, 16)
+        NSCOUNT = self.__getBinary(0, 16)
+        ARCOUNT = self.__getBinary(0, 16)
 
         temp = ID + QR + opcode + AA + TC + RD + RA + Z + \
             RCODE + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT
@@ -77,25 +84,25 @@ class dns_query:
 
         return header
 
-    def buildQuestion(self, domain_name):
+    def __buildQuestion(self):
         question_section = ""
-        partitions = domain_name.split('.')
+        partitions = self.domain_name.split('.')
 
         QNAME = ""
         for p in partitions:
-            QNAME += self.getBinary(len(p), 8)
+            QNAME += self.__getBinary(len(p), 8)
 
             for i in range(len(p)):
-                QNAME += self.getBinary(ord(p[i]), 8)
+                QNAME += self.__getBinary(ord(p[i]), 8)
 
         # end of the domain
-        QNAME += self.getBinary(0, 8)
+        QNAME += self.__getBinary(0, 8)
 
         # The DNS record type we’re looking up. We’ll be looking up A records, whose value is 1.
-        QTYPE = self.getBinary(1, 16)
+        QTYPE = self.__getBinary(1, 16)
 
         # The class we’re looking up. We’re using the the internet, IN, which has a value of 1
-        QCLASS = self.getBinary(1, 16)
+        QCLASS = self.__getBinary(1, 16)
 
         temp_question = QNAME + QTYPE + QCLASS
 
@@ -105,30 +112,10 @@ class dns_query:
             hx = hex(int(temp_question[i: i + 4], 2))
             question_section += hx[2:]
 
-        # for i in range(0, len(question_section), 2):
-        #     print(question_section[i : i + 2], end=" ")
-
         self.question_len = len(question_section)
-
         return question_section
 
-    def send_udp(self, msg):
-        # no blank or newline
-        msg = msg.replace(" ", "").replace("\n", "")
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        try:
-            sock.sendto(binascii.unhexlify(msg), (self.server_ip, self.port))
-            print("udp sent")
-
-            data, address = sock.recvfrom(4096)
-        finally:
-            sock.close()
-
-        return binascii.hexlify(data).decode("utf-8")
-
-    def parseResponse(self, res, showReport):
-
+    def __parseResponse(self, res):
         # ----------------------------------------
         # header
         head = []
@@ -144,15 +131,17 @@ class dns_query:
 
         # check id
         if(int(ID, 16) != self.query_no):
-            print("error : id not matched")
-            print("query-id :", self.query_no)
-            print("response-id :", int(ID, 16))
+            if self.showError:
+                print("error : id not matched")
+                print("query-id :", self.query_no)
+                print("response-id :", int(ID, 16))
+            
             return "-1"
 
         self.query_no += 1
 
         # check flags
-        flags = self.getBinary(int(flags, 16), 16)
+        flags = self.__getBinary(int(flags, 16), 16)
 
         # 1 4 1 1 1 1 3 4
         # |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
@@ -166,11 +155,13 @@ class dns_query:
         RCODE = flags[12: 16]
 
         if int(RCODE, 2) != 0:
-            print(RCODE, "error reported")
+            if self.showError:
+                print(RCODE, "error reported")
+            
             return "-1"
 
         # additional info
-        if showReport:
+        if self.showReport:
             print("No errors reported")
 
             if int(AA, 2) == 0:
@@ -199,7 +190,7 @@ class dns_query:
         RDLENGTH = int(answer_section[20: 24], 16)
         RDDATA = answer_section[24:]
 
-        if showReport:
+        if self.showReport:
             print("TTL :", int(TTL, 16), "seconds")
             print("RDLENGTH :", RDLENGTH, "bytes")
 
@@ -210,10 +201,12 @@ class dns_query:
         return ".".join(ip_adress_chunks)
         # ----------------------------------------
 
-    def getIP(self, domain_name, showReport):
-        header = self.buildHeader()
-        question = self.buildQuestion(domain_name)
-
-        res = self.send_udp(header + question)
-
-        return self.parseResponse(res, showReport)
+    def getDNSQuery(self):
+        if self.domain_name == "-1":
+            print("--- set domain name first !!! ---")
+            return
+        
+        return self.__buildHeader() + self.__buildQuestion()
+        
+    def getIP(self, res):
+        return self.__parseResponse(res)
--- testCode.py
+++ testCode.py
@@ -2,7 +2,27 @@
 
 from DNS_QueryBuilder import *
 
-q = dns_query("8.8.8.8", 53)
-ip = q.getIP("codeforces.com", True)
+def send_udp(msg ,server_ip, port):
+        # no blank or newline
+        msg = msg.replace(" ", "").replace("\n", "")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print(ip)
+        try:
+            sock.sendto(binascii.unhexlify(msg), (server_ip, port))
+            print("udp sent")
+
+            data, address = sock.recvfrom(4096)
+        finally:
+            sock.close()
+
+        return binascii.hexlify(data).decode("utf-8")
+    
+    
+q = dns_query()
+q.setShowError(True)
+q.setShowReport(True)
+q.setDomainName("codeforces.com")
+
+ques = q.getDNSQuery()
+res = send_udp(ques,"8.8.8.8", 53)
+print(q.getIP(res))  
